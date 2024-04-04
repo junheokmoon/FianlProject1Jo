4363a946bdea9854ac2c0dd9eb45795f277ddbcf
HonranE <wjd7871@naver.com>
2024-04-04 17:37:42 +0900

    payment, ticket, point, ottkind DTO 추가 및 수정 - 정준형(2024-04-04)

--- FinalProject/src/main/java/xyz/withy/dto/paymentDTO.java
+++ FinalProject/src/main/java/xyz/withy/dto/paymentDTO.java
@@ -4,26 +4,21 @@ import lombok.Builder;
 import lombok.Data;
 
 /*
-이름            널?       유형            
-------------- -------- ------------- 
-TICKET_CODE   NOT NULL VARCHAR2(10)  
-TICKET_PRICE  NOT NULL NUMBER        
-TICKET_IMAGE1          VARCHAR2(500) 
-TICKET_IMAGE2          VARCHAR2(500) 
-TICKET_OTT_NO          NUMBER        
-TICKET_MONTH  NOT NULL NUMBER        
-
+이름                  널?       유형      원래 이름
+------------------- -------- ------------ -------------
+PAYMENT_NO          NOT NULL NUMBER       결제 번호
+PAYMENT_USER_NO              NUMBER       회원 번호
+PAYMENT_TICKET_CODE          VARCHAR2(10) 결제 이용권 코드
+PAYMENT_DATE        NOT NULL DATE         결제 날짜
+PAYMENT_STATUS      NOT NULL NUMBER    	  결제 상태
 */
-
 @Data
 @Builder
 public class paymentDTO {
 
-	private String ticketCode;
-	private int ticketPrice;
-	private String ticketImage1;
-	private String ticketImage2;
-	private int ticketOttNo;
-	private int ticketMonth;
-	
+	private int paymentNo;
+	private int paymentUserNo;
+	private String paymentTicketCode;
+	private String paymentDate;
+	private int paymentStatus;
 }
--- FinalProject/src/main/java/xyz/withy/dto/ticketDTO.java
+++ FinalProject/src/main/java/xyz/withy/dto/ticketDTO.java
@@ -4,22 +4,25 @@ import lombok.Builder;
 import lombok.Data;
 
 /*
-이름                  널?       유형           
-------------------- -------- ------------ 
-PAYMENT_NO          NOT NULL NUMBER       
-PAYMENT_USER_NO              NUMBER       
-PAYMENT_TICKET_CODE          VARCHAR2(10) 
-PAYMENT_DATE        NOT NULL DATE         
-PAYMENT_STATUS      NOT NULL NUMBER    
+이름            널?       유형       원래 이름
+------------- -------- ------------- ----------------
+TICKET_CODE   NOT NULL VARCHAR2(10)  코드(종류/개월수)
+TICKET_PRICE  NOT NULL NUMBER        요금
+TICKET_IMAGE1          VARCHAR2(500) 이미지
+TICKET_IMAGE2          VARCHAR2(500) 이미지2
+TICKET_OTT_NO          NUMBER        종류
+TICKET_MONTH  NOT NULL NUMBER        개월수
 */
 
 @Data
 @Builder
 public class ticketDTO {
 
-	private int paymentNo;
-	private int paymentUserNo;
-	private String paymentTicketCode;
-	private String paymentDate;
-	private int paymentStatus;
+	private String ticketCode;
+	private int ticketPrice;
+	private String ticketImage1;
+	private String ticketImage2;
+	private int ticketOttNo; 
+	private int ticketMonth;
+	
 }
