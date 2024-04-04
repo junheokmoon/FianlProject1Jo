f9fb77bdc9a8dbdbcef16ec64c0fa9cb497edca6
HonranE <wjd7871@naver.com>
2024-04-04 20:33:57 +0900

    마저 수정했어요.

--- FinalProject/src/main/java/xyz/withy/dto/paymentDTO.java
+++ FinalProject/src/main/java/xyz/withy/dto/paymentDTO.java
@@ -11,7 +11,7 @@ PAYMENT_USER_NO              NUMBER       회원 번호
 PAYMENT_TICKET_CODE          VARCHAR2(10) 결제 이용권 코드
 PAYMENT_DATE        NOT NULL DATE         결제 날짜
 PAYMENT_STATUS      NOT NULL NUMBER    	  결제 상태
-*/ 
+*/
 @Data
 @Builder
 public class paymentDTO {
--- FinalProject/src/main/java/xyz/withy/dto/ticketDTO.java
+++ FinalProject/src/main/java/xyz/withy/dto/ticketDTO.java
@@ -11,8 +11,8 @@ TICKET_PRICE  NOT NULL NUMBER        요금
 TICKET_IMAGE1          VARCHAR2(500) 이미지
 TICKET_IMAGE2          VARCHAR2(500) 이미지2
 TICKET_OTT_NO          NUMBER        종류
-TICKET_MONTH  NOT NULL NUMBER        개월수 
-*/ 
+TICKET_MONTH  NOT NULL NUMBER        개월수
+*/
 
 @Data
 @Builder
