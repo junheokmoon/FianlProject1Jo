e3ff1f7384ec020dc25fe67c29a2b99960b30bcd
HonranE <wjd7871@naver.com>
2024-04-04 19:07:39 +0900

    수정해봤습니다.

--- FinalProject/src/main/java/xyz/withy/dto/ticketDTO.java
+++ FinalProject/src/main/java/xyz/withy/dto/ticketDTO.java
@@ -11,7 +11,7 @@ TICKET_PRICE  NOT NULL NUMBER        요금
 TICKET_IMAGE1          VARCHAR2(500) 이미지
 TICKET_IMAGE2          VARCHAR2(500) 이미지2
 TICKET_OTT_NO          NUMBER        종류
-TICKET_MONTH  NOT NULL NUMBER        개월수
+TICKET_MONTH  NOT NULL NUMBER        개월수 수정해봤습니다
 */ 
 
 @Data
