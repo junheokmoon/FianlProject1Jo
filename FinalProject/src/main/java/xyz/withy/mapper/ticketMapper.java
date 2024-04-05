f994999d08620cb9a7dd15053fb15eec033311b3
HonranE <wjd7871@naver.com>
2024-04-05 18:26:32 +0900

    ticketMapper 조금 수정했습니다. - 정준형

--- FinalProject/src/main/java/xyz/withy/mapper/ticketMapper.java
+++ FinalProject/src/main/java/xyz/withy/mapper/ticketMapper.java
@@ -1,5 +1,15 @@
 package xyz.withy.mapper;
 
-public interface ticketMapper {
+import java.util.List;
 
+import xyz.withy.dto.ticketDTO;
+
+public interface ticketMapper {
+	
+	int insertTicket(ticketDTO ticket);
+	int updateTicket(ticketDTO ticket);
+	int deleteTicket(ticketDTO ticket);
+	ticketDTO selectTicket(ticketDTO ticket);
+	int selectTicketCount();
+	List<ticketDTO> selectTicketList();
 }
