81f3350e129522d751c078bb9dc4aa3ce4f8b60a
11k9x <11k9x@DESKTOP-8ND4H5I>
2024-04-08 00:15:30 +0900

    Program Mapper/DAO 0408 정혜원

---
+++ withyFinal/src/main/java/xyz/withy/mapper/programMapper.java
@@ -0,0 +1,16 @@
+package xyz.withy.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import xyz.withy.dto.programDTO;
+
+public interface programMapper {
+	 int insertProgram(programDTO programDTO);
+	 int updateProgram(programDTO programDTO);
+	 int deleteProgram(int programNo);
+	 programDTO selectProgram(int programNo);
+	 int selectProgramCount();
+	 List<programDTO> selectRestBoardList(Map<String, Object> map);
+	//List<programDTO> selectProgramList();
+}
