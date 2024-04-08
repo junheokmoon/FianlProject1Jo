ce6a6b3e127d634c2383ac17f715d0ab03981e02
itwill <itwill@DESKTOP-CHQ2TR9>
2024-04-08 20:43:11 +0900

    mapper, dao에 이어서 컨트롤러 서비스 진행하기  - 김민준

--- withyFinal/src/main/java/xyz/withy/dao/InquiryDAOImpl.java
+++ withyFinal/src/main/java/xyz/withy/dao/InquiryDAOImpl.java
@@ -1,5 +1,48 @@
 package xyz.withy.dao;
 
-public class InquiryDAOImpl {
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+import xyz.withy.dto.InquiryDTO;
+import xyz.withy.mapper.InquiryMapper;
+
+@Repository
+@RequiredArgsConstructor
+public class InquiryDAOImpl implements InquiryDAO {
+	private final SqlSession sqlSession;
+	
+	@Override
+	public int insertInquiry(InquiryDTO inquiry) {
+		return sqlSession.getMapper(InquiryMapper.class).insertInquiry(inquiry);
+	}
+
+	@Override
+	public int updateInquiry(InquiryDTO inquiry) {
+		return sqlSession.getMapper(InquiryMapper.class).updateInquiry(inquiry);
+	}
+
+	@Override
+	public int deleteInquiry(int inquiryNo) {
+		return sqlSession.getMapper(InquiryMapper.class).deleteInquiry(inquiryNo);
+	}
+
+	@Override
+	public InquiryDTO selectInquiry(int inquiryNo) {
+		return sqlSession.getMapper(InquiryMapper.class).selectInquiry(inquiryNo);
+	}
+
+	@Override
+	public int selectInquiryCount() {
+		return sqlSession.getMapper(InquiryMapper.class).selectInquiryCount();
+	}
+
+	@Override
+	public List<InquiryDTO> selectInquiryList(Map<String, Object> map) {
+		return sqlSession.getMapper(InquiryMapper.class).selectInquiryList(map);
+	}
 
 }
--- withyFinal/src/main/java/xyz/withy/mapper/InquiryMapper.java
+++ withyFinal/src/main/java/xyz/withy/mapper/InquiryMapper.java
@@ -1,11 +1,11 @@
-package xyz.withy.dao;
+package xyz.withy.mapper;
 
 import java.util.List;
 import java.util.Map;
 
 import xyz.withy.dto.InquiryDTO;
 
-public interface InquiryDAO {
+public interface InquiryMapper {
 	int insertInquiry(InquiryDTO inquiry);
 	int updateInquiry(InquiryDTO inquiry);
 	int deleteInquiry(int inquiryNo);
