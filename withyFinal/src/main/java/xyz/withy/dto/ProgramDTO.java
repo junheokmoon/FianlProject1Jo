50437c8b4e454c7754b5a2150f11f562fd3471e6
itwill <itwill@DESKTOP-MV2KVS2>
2024-04-08 18:14:17 +0900

    Program Mapper/DTO 수정 0408 정혜원

--- withyFinal/src/main/java/xyz/withy/dto/ProgramDTO.java
+++ withyFinal/src/main/java/xyz/withy/dto/ProgramDTO.java
@@ -9,7 +9,6 @@ import lombok.Data;
 ------------------- -------- -------------
 PROGRAM_NO          NOT NULL NUMBER        프로그램번호
 PROGRAM_OTT_NO      NOT NULL NUMBER(38)    프로그램OTT 번호(FK)
-PROGRAM_AGE                  NUMBER(5)     프로그램 연령제한
 PROGRAM_NAME        NOT NULL VARCHAR2(30)  프로그램 이름
 PROGRAM_IMAGE                VARCHAR2(500) 프로그램 이미지
 PROGRAM_DETAIL      NOT NULL VARCHAR2(200) 프로그램 설명
@@ -24,7 +23,6 @@ PROGRAM_CATEGORY_NO          NUMBER(5)     프로그램 세부 카테고리
 public class ProgramDTO {
 	private int programNo;
 	private int programOttNo;
-	private int programAge;
 	private String programName;
 	private String programImage;
 	private String programDetail;
