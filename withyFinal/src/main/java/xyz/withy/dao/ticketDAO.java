31e78fbc5e7d36f30ca7532206934a66ab7f8563
HonranE <wjd7871@naver.com>
2024-04-08 18:11:39 +0900

    ticket dao 완료 - 정준형

--- withyFinal/src/main/java/xyz/withy/dao/ticketDAO.java
+++ withyFinal/src/main/java/xyz/withy/dao/ticketDAO.java
@@ -1,5 +1,19 @@
 package xyz.withy.dao;
 
-public interface ticketDAO {
+import java.util.List;
+import java.util.Map;
 
+import xyz.withy.dto.ticketDTO;
+
+public interface ticketDAO {
+	
+	int insertTicket(ticketDTO ticket); // 추가
+	int updateTicket(ticketDTO ticket); // 업데이트
+	int deleteTicket(String ticketCode); // 사실상 status 2번으로 변경
+	int realDeleteTicket(String ticketCode); //진짜 순도 100% delete
+	int recoverTicket(String ticketCode); // 사실상 status 1번으로 변경
+	ticketDTO selectTicket(String ticketCode); // 그냥 검색
+	int selectTicketCount(); //티켓의 전체 갯수
+	List<ticketDTO> selectTicketList(); //전체 목록 보기
+	List<ticketDTO> selectTicketPageList(Map<String, Object> map); // 페이징 처리된 리스트
 }
--- withyFinal/src/main/java/xyz/withy/dao/ticketDAOImpl.java
+++ withyFinal/src/main/java/xyz/withy/dao/ticketDAOImpl.java
@@ -1,5 +1,65 @@
 package xyz.withy.dao;
 
-public class ticketDAOImpl { 
+import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+import xyz.withy.dto.ticketDTO;
+import xyz.withy.mapper.ticketMapper;
+
+@Repository
+@RequiredArgsConstructor
+public class ticketDAOImpl implements ticketDAO{
+
+	private final SqlSession sqlSession;
+	
+	@Override
+	public int insertTicket(ticketDTO ticket) {
+		return sqlSession.getMapper(ticketMapper.class).insertTicket(ticket);
+	}
+
+	@Override
+	public int updateTicket(ticketDTO ticket) {
+		return sqlSession.getMapper(ticketMapper.class).updateTicket(ticket);
+	}
+
+	@Override
+	public int deleteTicket(String ticketCode) {
+		return sqlSession.getMapper(ticketMapper.class).deleteTicket(ticketCode);
+	}
+
+	@Override
+	public int realDeleteTicket(String ticketCode) {
+		return sqlSession.getMapper(ticketMapper.class).realDeleteTicket(ticketCode);
+	}
+
+	@Override
+	public int recoverTicket(String ticketCode) {
+		return sqlSession.getMapper(ticketMapper.class).recoverTicket(ticketCode);
+	}
+
+	@Override
+	public ticketDTO selectTicket(String ticketCode) {
+		return sqlSession.getMapper(ticketMapper.class).selectTicket(ticketCode);
+	}
+
+	@Override
+	public int selectTicketCount() {
+		return sqlSession.getMapper(ticketMapper.class).selectTicketCount();
+	}
+
+	@Override
+	public List<ticketDTO> selectTicketList() {
+		return sqlSession.getMapper(ticketMapper.class).selectTicketList();
+	}
+
+	@Override
+	public List<ticketDTO> selectTicketPageList(Map<String, Object> map) {
+		return sqlSession.getMapper(ticketMapper.class).selectTicketPageList(map);
+	} 
+
+	
 }
